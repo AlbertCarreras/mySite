f34db382979817c4e37f276d3ec9075eb82f3b0a
Alberto Carreras <a.carreras.c@gmail.com>
2019-03-17 16:42:19 -0400

    Updates

---
+++ precache-manifest.fb8b2caa0e9a954121a41223461d1334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a96ea38d58fd9929229dfc8659d19660",
+    "url": "/mySite/static/media/foto.a96ea38d.JPG"
+  },
+  {
+    "revision": "231b9cfd08ca0436e83d",
+    "url": "/mySite/static/js/runtime~main.f09b6384.js"
+  },
+  {
+    "revision": "9fd0b9687f6aa4656b41",
+    "url": "/mySite/static/js/main.7fa26ab9.chunk.js"
+  },
+  {
+    "revision": "f05a1df3217379e807f2",
+    "url": "/mySite/static/js/2.fba19b92.chunk.js"
+  },
+  {
+    "revision": "9fd0b9687f6aa4656b41",
+    "url": "/mySite/static/css/main.6a8c71e7.chunk.css"
+  },
+  {
+    "revision": "46ec45b48b167224e57ffeafe56878e2",
+    "url": "/mySite/index.html"
+  }
+];
