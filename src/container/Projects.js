bc0c52e09ef7680ee2817190febe6c5b29a53dee
Alberto Carreras <a.carreras.c@gmail.com>
2019-04-22 17:19:20 -0400

    added iex app

--- src/container/Projects.js
+++ src/container/Projects.js
@@ -5,6 +5,154 @@ class About extends Component {
     return (
       <div className='sub-container detail-container project-container'>
           <div>
+            <div className='project-title'>IEX Trading App</div>
+            <div className='project-links'>
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/IEXapp/tree/master/iex_app_react"
+              target="_blank"
+              rel="noopener noreferrer"  
+              >GITHUB Front-end</a> | 
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/IEXapp/tree/master/iex_app_api"
+              target="_blank"
+              rel="noopener noreferrer"
+              >GITHUB Back-end</a>
+            </div>
+            <div className='section'>A trade-wallet simulator app to search, buy, and sell stock shares using the Investors Exchange (IEX) API.</div>
+            <div className='video-container'>
+
+              <iframe 
+                title='IEX' 
+                src="https://www.youtube-nocookie.com/embed/qFopD7s0BEw" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+              </iframe>
+              
+            </div> 
+            <div>
+              <div className='subtitle'>Front-end</div>
+                <ul>
+                  <li>React with Redux & Redux Thunk</li>
+                  <li>D3 for chart visualization</li>
+                </ul>
+                <div className='subtitle'>Back-end</div>
+                <ul>
+                  <li>Rails API with with serialization and Postgres
+                  </li>
+                </ul>
+              </div>
+          </div>
+          <div>
+            <div className='project-title'>SUPP</div>
+            <div className='project-links'>
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/supp-client"
+              target="_blank"
+              rel="noopener noreferrer"  
+              >GITHUB Front-end</a> | 
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/supp-server"
+              target="_blank"
+              rel="noopener noreferrer"
+              >GITHUB Back-end</a> | 
+              <a 
+              className='links'
+              href="https://albertcarreras.github.io/supp-client/home"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Site </a>
+            </div>
+            <div
+            className='project-note'
+            >Hosted in Heroku, wait a few seconds until the server wakes up! (user: tester & password: 12345678)</div>
+            <div className='section'>A user-friendly app that sorts users by distance and filters them by common interests/hobbies. Users connect with each other via private chats.</div>
+            <div className='video-container'>
+              <iframe 
+                title='SUPP' 
+                src="https://www.youtube-nocookie.com/embed/yG3gEc1bC60" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+              </iframe>
+            </div> 
+            <div>
+              <div className='subtitle'>Front-end</div>
+                <ul>
+                  <li>React with Redux & Redux Thunk</li>
+                  <li>Geolocation Web API for obtaining user’s current location coordinates</li>
+                  <li>Custom CSS, SASS, and Semantic UI elements for front-end design</li>
+                  <li>Responsive and web first design</li>
+                </ul>
+                <div className='subtitle'>Back-end</div>
+                <ul>
+                  <li>Rails API with with serialization and Postgres
+                  </li>
+                  <li>Active Storage for photo storage connected to AWS S3 in production</li>
+                  <li>Action Cable for live private chat feed updating and connected-user indicators</li>
+                  <li>Knock for JSON Web Token authentication and cookies for websocket-connection authentication</li>
+                  <li>Geokit for calculating surrounding users to connected user distance</li>
+                </ul>
+              </div>
+          </div>
+          <div>
+            <div className='project-title'>SUPP</div>
+            <div className='project-links'>
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/supp-client"
+              target="_blank"
+              rel="noopener noreferrer"  
+              >GITHUB Front-end</a> | 
+              <a 
+              className='links'
+              href="https://github.com/AlbertCarreras/supp-server"
+              target="_blank"
+              rel="noopener noreferrer"
+              >GITHUB Back-end</a> | 
+              <a 
+              className='links'
+              href="https://albertcarreras.github.io/supp-client/home"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Site </a>
+            </div>
+            <div
+            className='project-note'
+            >Hosted in Heroku, wait a few seconds until the server wakes up! (user: tester & password: 12345678)</div>
+            <div className='section'>A user-friendly app that sorts users by distance and filters them by common interests/hobbies. Users connect with each other via private chats.</div>
+            <div className='video-container'>
+              <iframe 
+                title='SUPP' 
+                src="https://www.youtube-nocookie.com/embed/yG3gEc1bC60" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+              </iframe>
+            </div> 
+            <div>
+              <div className='subtitle'>Front-end</div>
+                <ul>
+                  <li>React with Redux & Redux Thunk</li>
+                  <li>Geolocation Web API for obtaining user’s current location coordinates</li>
+                  <li>Custom CSS, SASS, and Semantic UI elements for front-end design</li>
+                  <li>Responsive and web first design</li>
+                </ul>
+                <div className='subtitle'>Back-end</div>
+                <ul>
+                  <li>Rails API with with serialization and Postgres
+                  </li>
+                  <li>Active Storage for photo storage connected to AWS S3 in production</li>
+                  <li>Action Cable for live private chat feed updating and connected-user indicators</li>
+                  <li>Knock for JSON Web Token authentication and cookies for websocket-connection authentication</li>
+                  <li>Geokit for calculating surrounding users to connected user distance</li>
+                </ul>
+              </div>
+          </div>
+          <div>
             <div className='project-title'>SUPP</div>
             <div className='project-links'>
               <a 
